1d128c13ae205dd8ca9e44278f0e0d340d7ed368
Cassiolucianoo <cassiolucianodasilva@gmail.com>
2021-04-30 14:38:25 -0300

    Consulta 2
    Passando como parametro nome PreparedStatement

--- exercicios/src/jdbc/CunsultarPesssoas02.java
+++ exercicios/src/jdbc/CunsultarPesssoas02.java
@@ -1,66 +1,55 @@
 package jdbc;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class CunsultarPesssoasDesafio {
+public class CunsultarPesssoas02 {
 
 	public static void main(String[] args) throws SQLException {
-
-		String nomeinformado;
-		int quantUser = 0;
-
-		// A Scannerclasse é usada para obter a entrada do usuário e é encontrada no
-		// java.utilpacote.
+		
+		String nomePesquisa = null;
 		Scanner entrada = new Scanner(System.in);
 
 		System.out.println("Infrome O nome: ");
-		nomeinformado = entrada.next();
+		nomePesquisa = entrada.next();
 
 		Connection conexao = Conexao.getConexao();
-		String sql = "SELECT * FROM pessoas where (nome) like ('" + nomeinformado + "')";
+		String sql = "SELECT * FROM pessoas where nome like ?";
 
-		Statement stmt = conexao.createStatement();
-
-		// resultados do banco de dados para o java
-		ResultSet resultado = stmt.executeQuery(sql);
+		PreparedStatement stmt = conexao.prepareStatement(sql);
+		stmt.setString(1, "%"+ nomePesquisa +"%");
+		
+		//resultados do banco de dados para o java
+		ResultSet resultado = stmt.executeQuery();
 
 		List<Pessoa> pessoas = new ArrayList<>();
 
-		// O metodo next ele retorna um valor true ou false então ele anda na linsta até
-		// que retorne um false
+		
+		//O metodo next ele retorna um valor true ou false então ele anda na linsta até que retorne um false
 		while (resultado.next()) {
 
 			int codigo = resultado.getInt("codigo");
 			String nome = resultado.getString("nome");
 			String sobrenome = resultado.getString("sobre_nome");
 			int idade = resultado.getInt("idade");
-
-			// com todos os atributos do banco foi criado uma pessoa
+			
+            // com todos os atributos do banco foi criado uma pessoa
 			pessoas.add(new Pessoa(codigo, nome, sobrenome, idade));
-
-			quantUser = quantUser + 1;
-
 		}
-		// aqui estou percorrendo a lista pessoas
+        // aqui estou percorrendo a lista pessoas
 		for (Pessoa p : pessoas) {
-
 			System.out.println("Codi : " + p.getCodigo() + "\n" + "Nome :" + p.getNome() + " " + p.getSobrenome() + "\n"
 					+ "Idade :" + p.getIdade() + "\n");
-
 		}
 		
-        System.out.println("Pessoas encontradas na tabela =  "+ quantUser);
-        
 		stmt.close();
 		conexao.close();
 		entrada.close();
-
 	}
 
 }
