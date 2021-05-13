225741790f5c290eb37860c01750fc5686ad516f
Cassiolucianoo <cassiolucianodasilva@gmail.com>
2021-05-13 04:22:23 -0300

    Construção loja view
    Controller
    dao
    factory
    modelo
    view

---
+++ loja-view-repository/src/br/com/loja/jdbc/controller/CategoriaController.java
@@ -0,0 +1,16 @@
+package br.com.loja.jdbc.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.loja.jdbc.modelo.Categoria;
+
+public class CategoriaController {
+
+	public List<Categoria> listar() {
+		List<Categoria> categorias = 
+				new ArrayList<Categoria>();
+		categorias.add(new Categoria(1, "Categoria de teste")); 
+		return categorias;
+	}
+}
