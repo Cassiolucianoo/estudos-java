9f58a9b66fed6145e1710f2019b6552110055077
Cassio Luciano da silva <37276985+Cassiolucianoo@users.noreply.github.com>
2023-06-24 19:19:02 -0300

    simplificando criação de objeto e aplicando validation anotação

--- exercicios-sb/src/main/java/br/com/cassiolucianodasilva/exerciciossb/controllers/ProdutoController.java
+++ exercicios-sb/src/main/java/br/com/cassiolucianodasilva/exerciciossb/controllers/ProdutoController.java
@@ -1,5 +1,7 @@
 package br.com.cassiolucianodasilva.exerciciossb.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,23 +12,36 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.cassiolucianodasilva.exerciciossb.model.entities.Produto;
 import br.com.cassiolucianodasilva.exerciciossb.model.repositories.ProdutoRepository;
 
-
-
 @RestController
 @RequestMapping("/api/produtos")
 public class ProdutoController {
 
-	@Autowired
+	// private final ProdutoRepository produtoRepository;
+
+	// public ProdutoController(ProdutoRepository produtoRepository) {
+	// this.produtoRepository = produtoRepository;
+	// }
+
+	 @Autowired
 	private ProdutoRepository produtoRepository;
 
-	@PostMapping
-	public @ResponseBody Produto novoProdutos(@RequestParam String nome, @RequestParam Float preco) {
+//	@PostMapping
+//	public @ResponseBody Produto novoProdutos(@RequestParam String nome,
+//			@RequestParam Float preco, @RequestParam float desconto) {
+//
+//		Produto produto = new Produto(nome, preco, desconto);
+//		produtoRepository.save(produto);
+//		return produto;
+//
+//	}
 
-		Produto produto = new Produto(nome, preco);
-	produtoRepository.save(produto);
-		return produto;
+	// Simplificando recebendo o objeto como parametros par simplificar e passando para save
 
-	}
+	 @PostMapping
+		public @ResponseBody Produto novoProdutos(@Valid Produto produto) {
+			produtoRepository.save(produto);
+			return produto;
 
+		}
+	  
 }
- 	
--- exercicios-sb/src/main/java/br/com/cassiolucianodasilva/exerciciossb/model/entities/Produto.java
+++ exercicios-sb/src/main/java/br/com/cassiolucianodasilva/exerciciossb/model/entities/Produto.java
@@ -4,6 +4,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 public class Produto {
@@ -11,27 +14,55 @@ public class Produto {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
-	
-	private String nome;
-	private float valor;
-	
-	
-	public Produto( ) {
-	
-	}
-	public Produto( String nome, float valor) {
-		super();
 
+	@NotBlank
+	private String nome;
+	
+	@Min(0)
+	private double valor;
+	
+	@Min(0)
+	@Max(1)
+	private double desconto;
+
+	public Produto() {
+
+	}
+
+	public Produto(String nome, double valor, double desconto) {
+		super();
+		this.desconto = desconto;
 		this.nome = nome;
 		this.valor = valor;
 	}
 
-	public float getValor() {
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getDesconto() {
+
+		return desconto;
+	}
+
+	public void setDesconto(double desconto) {
+
+		this.desconto = desconto;
+
+	}
+
+	public double getValor() {
 		return valor;
 	}
 
-	public void setValor(float valor) {
-		this.valor = valor;
+	public void setValor(double valor) {
+
+		this.valor = this.desconto;
+
 	}
 
 	public int getId() {
@@ -40,14 +71,6 @@ public class Produto {
 
 	public void setId(int id) {
 		this.id = id;
-	}
-
-	public String getName() {
-		return nome;
-	}
-
-	public void setName(String name) {
-		this.nome = name;
 	}
 
 }
