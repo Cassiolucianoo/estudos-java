11b85644aef848e382262f0919987bd639957b15
Cassio Luciano da silva <37276985+Cassiolucianoo@users.noreply.github.com>
2023-06-25 10:20:20 -0300

    consulta paginada Pageable

--- exercicios-sb/src/main/java/br/com/cassiolucianodasilva/exerciciossb/model/repositories/ProdutoRepository.java
+++ exercicios-sb/src/main/java/br/com/cassiolucianodasilva/exerciciossb/model/repositories/ProdutoRepository.java
@@ -1,11 +1,16 @@
 package br.com.cassiolucianodasilva.exerciciossb.model.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import br.com.cassiolucianodasilva.exerciciossb.model.entities.Produto;
 
 //Declaração da interface ProdutoRepository
 
-public interface ProdutoRepository extends CrudRepository<Produto, Integer> {
+//public interface ProdutoRepository extends CrudRepository<Produto, Integer> {
+//
+//}
+
+public interface ProdutoRepository extends PagingAndSortingRepository<Produto, Integer> {
 
 }
